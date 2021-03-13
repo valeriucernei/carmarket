237e91a746b30f11b13709f4b7e6fe5356616f4b
valeriucernei <cernei.valeriu99@gmail.com>
2021-03-13 19:55:36 +0100

    Create config file

---
+++ api/config.php
@@ -0,0 +1,11 @@
+<?php
+
+class Config {
+  const DB_HOST = "localhost";
+  const DB_USERNAME = "carmarket";
+  const DB_PASSWORD = "123321";
+  const DB_SCHEME = "carmarket";
+  const DB_PORT = "3307";
+}
+
+?>
