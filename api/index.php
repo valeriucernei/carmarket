d671c192e8f87603bdc9c80ce6a824045d542976
valeriucernei <cernei.valeriu99@gmail.com>
2021-03-15 19:39:01 +0100

    Small code refactoring

--- api/index.php
+++ api/index.php
@@ -12,18 +12,41 @@ Flight::route('/', function(){
     echo 'hello world!';
 });
 
+
+
+/**
+ * API Return all users in JSON
+ */
 Flight::route('GET /users', function(){
     flight::json(Flight::userdao()->get_all());
 });
 
+
+
+/**
+ * API Return users data in JSON by ID
+ * @var int User ID
+ */
 Flight::route('GET /users/@id', function($id){
     flight::json(Flight::userdao()->get_by_id($id));
 });
 
+
+
+/**
+ * API Add new user in Data Base, and return user ID
+ * @var [type]
+ */
 Flight::route('POST /users', function(){
     Flight::json(Flight::userdao()->add(Flight::request()->data->getData()));
 });
 
+
+
+/**
+ * Update user data in Data Base by ID
+ * @var int User ID
+ */
 Flight::route('PUT /users/@id', function($id){
     flight::json(Flight::userdao()->get_by_id($id));
 
@@ -31,7 +54,7 @@ Flight::route('PUT /users/@id', function($id){
     Flight::json(Flight::request()->data->getData());
 });
 
+
+
 Flight::start();
-
-
 ?>
