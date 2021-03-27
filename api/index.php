0dcc59463c2a2fb525d01d593ee27227b0fff18c
valeriucernei <cernei.valeriu99@gmail.com>
2021-03-27 20:46:34 +0100

    small fix

--- api/index.php
+++ api/index.php
@@ -2,6 +2,7 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
+  
 
   require_once dirname(__FILE__)."/../vendor/autoload.php";
   require_once dirname(__FILE__)."/services/UserService.class.php";
