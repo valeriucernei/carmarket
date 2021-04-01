9a868d7506e52df896872a31bd58144d802a82bd
valeriucernei <cernei.valeriu99@gmail.com>
2021-04-01 14:46:17 +0200

    other middleware option

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -1,5 +1,5 @@
 <?php
-
+  /*FILTER BASED MIDDLEWARE*//*
   Flight::before('start', function($params, $output){
     if(Flight::request()->url =='/swagger') return TRUE;
     //if(str_starts_with(Flight::request()->url, '/users/')) return TRUE;
@@ -13,6 +13,22 @@
       Flight::json(["message" => $e->getMessage()], 401);
       die;
     }
-  });
+  });*/
+
+    /*ROUTE BASED MIDDLEWARE*/
+    Flight::route('*', function(){
+      if(Flight::request()->url =='/swagger') return TRUE;
+      if(str_starts_with(Flight::request()->url, '/users/login')) return TRUE;
+      $headers = getallheaders();
+      $token = @$headers["Authentication"];
+      try{
+        $decoded = (array)\Firebase\JWT\JWT::decode($token, "?8JwAt8>&M3JYX}nky+=*N#V,pbW9Tz.", array('HS256'));
+        Flight::set('user', $decoded);
+        return TRUE;
+      }catch (\Exception $e) {
+        Flight::json(["message" => $e->getMessage()], 401);
+        die;
+      }
+    });
 
 ?>
